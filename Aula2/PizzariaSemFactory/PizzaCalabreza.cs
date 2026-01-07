6da9b081cac10b12eff3990ac939320350eef60c
Fábio Nunes <fabionunesbr@gmail.com>
2026-01-06 21:52:58 -0300

    first commit

---
+++ Aula2/PizzariaSemFactory/PizzaCalabreza.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzariaSemFactory
+{
+    public class PizzaCalabreza
+    {
+        public string Nome { get; set; }
+
+        public PizzaCalabreza()
+        {
+            Nome = "Pizza de Calabresa";
+        }
+
+        public void Preparar()
+        {
+            Console.WriteLine($"\nPreparando {Nome}...");
+        }
+
+        public void Assar(int tempo)
+        {
+            Console.WriteLine($"\n{Nome} assando por {tempo} min.");
+        }
+
+        public void Embalar()
+        {
+            Console.WriteLine($"\nEmbalando {Nome}...\n");
+        }
+    }
+}
---
+++ Aula2/PizzariaSemFactory/Pizzaria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzariaSemFactory
+{
+    public class Pizzaria
+    {
+        public static void SolicitaPizza()
+        {
+            PizzaCalabreza pizzaCalabreza;
+            PizzaMussarela pizzaMussarela;
+            Console.WriteLine("Bem vindo a Pizzaria Sem Factory!");
+            Console.WriteLine("Qual pizza deseja solicitar? (1 - Calabresa, 2 - Mussarela)");
+            var pizzaEscolhida = Console.ReadLine();
+
+            if (pizzaEscolhida == "1")
+            {
+                pizzaCalabreza = new PizzaCalabreza();
+                pizzaCalabreza.Preparar();
+                pizzaCalabreza.Assar(30);
+                pizzaCalabreza.Embalar();
+            }
+            else if (pizzaEscolhida == "2")
+            {
+                pizzaMussarela = new PizzaMussarela();
+                pizzaMussarela.Preparar();
+                pizzaMussarela.Assar(25);
+                pizzaMussarela.Embalar();
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida. Por favor, escolha 1 ou 2.");
+            }
+        }
+
+
+    }
+}
